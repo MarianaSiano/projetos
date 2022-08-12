6550867414271147a8100f7c8f7cce31b224b5a6
Mariana <maryhsiano@hotmail.com>
2022-08-12 13:58:38 -0300

    Adicionando novos arquivos

---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+//Importe React
+import React, { Component } from "react";
+
+//Importe css
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className="calculator"></div>
+        )
+    }
+}
